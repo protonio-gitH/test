9ef43e911e80c17d65e2d88c058bc67c793b3046
protonio-gitH <blinovkirill321@gmail.com>
2026-08-31 01:47:17 +0300

    add prisma models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller.js';
+import { AppResolver } from './app.resolver.js';
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
