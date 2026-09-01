35ea9dbe2701a02109f8f3b250ce5a1dd09cba6d
protonio-gitH <blinovkirill321@gmail.com>
2026-09-01 17:22:31 +0300

    returned dotenv/config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  console.log('DATABASE_URL:', process.env.DATABASE_URL ? process.env.DATABASE_URL : 'MISSING');
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
